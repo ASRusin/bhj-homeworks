180f74e397a6a5158b682fca60f1a829a9a3f9e3
Alexandr Rusin <Asr.i@mail.ru>
2020-11-29 13:40:49 +0300

    Доработана третья задача

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -4,50 +4,35 @@ const sliderItem = [...document.getElementsByClassName("slider__item")];
 const sliderDots = [...document.getElementsByClassName("slider__dot")];
 let numSlide = 0;
 
+function isActive(item) {
+  sliderItem[numSlide].className = "slider__item";
+  sliderDots[numSlide].className = "slider__dot";
+  numSlide = item;
+  sliderItem[numSlide].className = "slider__item slider__item_active";
+  sliderDots[numSlide].className = "slider__dot slider__dot_active";
+}
+
 arrowNext.onclick = () => {
-  if (sliderItem[numSlide].className.includes("slider__item slider__item_active")) {
-    if (numSlide < sliderItem.length - 1) {
-      sliderItem[numSlide].className = "slider__item";
-      sliderDots[numSlide].className = "slider__dot";
-      sliderItem[numSlide + 1].className = "slider__item slider__item_active";
-      sliderDots[numSlide + 1].className = "slider__dot slider__dot_active";
-      numSlide++;
-    } else {
-      sliderItem[numSlide].className = "slider__item";
-      sliderDots[numSlide].className = "slider__dot";
-      numSlide = 0;
-      sliderItem[numSlide].className = "slider__item slider__item_active";
-      sliderDots[numSlide].className = "slider__dot slider__dot_active";
-    }
+  if (numSlide < sliderItem.length - 1) {
+    isActive(numSlide + 1);
+  } else {
+    isActive(0);
   }
 };
 
 arrowPrev.onclick = () => {
-  if (sliderItem[numSlide].className.includes("slider__item slider__item_active")) {
-    if (numSlide > 0) {
-      sliderItem[numSlide].className = "slider__item";
-      sliderDots[numSlide].className = "slider__dot";
-      sliderItem[numSlide - 1].className = "slider__item slider__item_active";
-      sliderDots[numSlide - 1].className = "slider__dot slider__dot_active";
-      numSlide--;
-    } else {
-      sliderItem[numSlide].className = "slider__item";
-      sliderDots[numSlide].className = "slider__dot";
-      numSlide = sliderItem.length - 1;
-      sliderItem[numSlide].className = "slider__item slider__item_active";
-      sliderDots[numSlide].className = "slider__dot slider__dot_active";
-    }
+  if (numSlide > 0) {
+    isActive(numSlide - 1);
+  } else {
+    isActive(sliderItem.length - 1);
   }
 };
 
 for (let i = 0; i < sliderDots.length; i++) {
-  sliderDots[i].onclick = () => {
-    if (sliderDots[i].className.includes("slider__item slider__item_active") === false) {
-      sliderItem[numSlide].className = "slider__item";
-      sliderDots[numSlide].className = "slider__dot";
-      numSlide = i;
-      sliderItem[numSlide].className = "slider__item slider__item_active";
-      sliderDots[numSlide].className = "slider__dot slider__dot_active";
-    }
-  };
+  sliderDots[i].onclick = () => isActive(i);
 }
+
+// Из интереса (клик по картинке):
+
+const sliderImg = document.querySelector(".slider__arrows");
+sliderImg.onclick = arrowNext.onclick;
