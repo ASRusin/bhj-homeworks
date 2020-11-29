837fdc4dde47584ea7c17aea2551112366387b21
Alexandr Rusin <Asr.i@mail.ru>
2020-11-29 12:41:12 +0300

    Доработал первую задачу

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,13 +1,19 @@
-const menu = document.querySelectorAll('.menu__link');
+const menu = document.querySelectorAll(".menu__link");
 const len = menu.length;
-for(let i = 0; i < len; i++) {   
-    menu[i].onclick = function() { 
-        const menuActive = document.querySelector('.menu_active');
-        if (menuActive) menuActive.className = 'menu menu_sub';
-        const menuItem = menu[i].closest('.menu__item');        
-        if (menuItem.querySelector('.menu .menu_sub')) {
-            menuItem.querySelector('.menu .menu_sub').className = 'menu menu_sub menu_active';
-            return false;
-        }
+for (let i = 0; i < len; i++) {
+  const menuItem = menu[i].closest(".menu__item");
+  menu[i].onclick = function () {
+    const menuActive = document.querySelector(".menu_active");
+
+    if (menuItem.querySelector(".menu_active")) {
+      menuItem.querySelector(".menu .menu_sub").className = "menu menu_sub";
+      return false;
+    } else if (menuItem.querySelector(".menu .menu_sub")) {
+      menuItem.querySelector(".menu .menu_sub").className = "menu menu_sub menu_active";
+      if (menuActive) {
+        menuActive.className = "menu menu_sub";
+      }
+      return false;
     }
+  };
 }
