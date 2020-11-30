f169a47c80ad2d46f03a92e9464cbda47b0511b6
Alexandr Rusin <Asr.i@mail.ru>
2020-11-30 22:50:27 +0300

    Первая задача сделана

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,24 @@
+const dropdownValue = [...document.getElementsByClassName("dropdown__value")];
+const dropdownList = [...document.getElementsByClassName("dropdown__list")];
+const dropdownItems = [...document.getElementsByClassName("dropdown__link")];
+
+for (let i = 0; i < dropdownValue.length; i++) {
+  dropdownValue[i].addEventListener("click", () => {
+    for (let list of dropdownList) {
+      if (list.className.includes("dropdown__list_active")) {
+        list.className = "dropdown__list";
+      }
+    };
+    if (dropdownList[i].className.includes("dropdown__list_active") === false) {
+      dropdownList[i].className = "dropdown__list dropdown__list_active";
+    }
+  });
+}
+
+for (let i = 0; i < dropdownItems.length; i++) {
+  dropdownItems[i].addEventListener("click", function (e) {
+    e.preventDefault();
+    this.closest(".dropdown").querySelector(".dropdown__value").textContent = this.textContent;
+    this.closest(".dropdown").querySelector(".dropdown__list").className = "dropdown__list";
+  });
+}
