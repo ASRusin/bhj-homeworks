e0b42c04aab1b2b5b024dea5ebf656a0ef2bd720
Alexandr Rusin <Asr.i@mail.ru>
2020-12-19 12:40:20 +0300

    Все задачи решены

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,18 @@
+const tooltips = [...document.getElementsByClassName("has-tooltip")];
+
+let hasTooltip = document.createElement("div");
+hasTooltip.className = "tooltip";
+tooltips[0].appendChild(hasTooltip);
+
+for (let i = 0; i < tooltips.length; i++) {
+  tooltips[i].addEventListener("click", (e) => {
+    e.preventDefault();
+    const left = tooltips[i].getBoundingClientRect().left;
+    const top = tooltips[i].getBoundingClientRect().bottom;    
+    hasTooltip.className = "tooltip tooltip_active";
+    hasTooltip.textContent = `${tooltips[i].getAttribute("title")}`;
+    hasTooltip.style.left = left + "px";
+    hasTooltip.style.top = top + "px";
+    
+  });
+}
