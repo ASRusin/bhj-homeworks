3b02970b0245df7f42e35a8cdaaa2f7495ead825
Alexandr Rusin <Asr.i@mail.ru>
2020-12-19 16:03:33 +0300

    Задача исправлена

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -8,11 +8,14 @@ for (let i = 0; i < tooltips.length; i++) {
   tooltips[i].addEventListener("click", (e) => {
     e.preventDefault();
     const left = tooltips[i].getBoundingClientRect().left;
-    const top = tooltips[i].getBoundingClientRect().bottom;    
-    hasTooltip.className = "tooltip tooltip_active";
+    const top = tooltips[i].getBoundingClientRect().bottom;
+    if (hasTooltip.textContent === tooltips[i].getAttribute("title")) {
+      hasTooltip.className = "tooltip";
+    } else {
+      hasTooltip.className = "tooltip tooltip_active";
+    }
     hasTooltip.textContent = `${tooltips[i].getAttribute("title")}`;
     hasTooltip.style.left = left + "px";
     hasTooltip.style.top = top + "px";
-    
   });
 }
